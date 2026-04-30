a2c41456299f252f9ec2ff58604918bcd142dfac
Irfan Mohmand <irfanmohmand987@gmail.com>
2026-04-30 12:17:52 +0500

    suspense added

---
+++ src/app/(pages)/verify-email/VerifyEmailClient.jsx
@@ -0,0 +1,37 @@
+"use client";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+
+const VerifyEmailClient = () => {
+  const params = useSearchParams();
+
+  const verify = async () => {
+    try {
+      const token = params.get("token");
+      const res = await axios.get(`/api/verify-email?token=${token}`);
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.error(error.response?.data?.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="verify flex justify-center bg-gray-600 flex-col gap-4 items-center w-full h-screen">
+        <h1 className="text-3xl text-white font-black ">
+          Please click to verify your email...
+        </h1>
+        <button
+          onClick={() => verify()}
+          className="px-12 font-bold py-2 bg-blue-700 rounded-lg text-white cursor-pointer "
+        >
+          Verify
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default VerifyEmailClient;
--- src/app/(pages)/verify-email/page.jsx
+++ src/app/(pages)/verify-email/page.jsx
@@ -1,37 +1,12 @@
-"use client";
-import axios from "axios";
-import { useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
-import toast from "react-hot-toast";
+import React, { Suspense } from "react";
+import VerifyEmailClient from "./VerifyEmailClient";
 
-const VerifyEmail = () => {
-  const params = useSearchParams();
-
-  const verify = async () => {
-    try {
-      const token = params.get("token");
-      const res = await axios.get(`/api/verify-email?token=${token}`);
-      toast.success(res.data.message);
-    } catch (error) {
-      toast.error(error.response?.data?.message);
-    }
-  };
-
+const page = () => {
   return (
-    <>
-      <div className="verify flex justify-center bg-gray-600 flex-col gap-4 items-center w-full h-screen">
-        <h1 className="text-3xl text-white font-black ">
-          Please click to verify your email...
-        </h1>
-        <button
-          onClick={() => verify()}
-          className="px-12 font-bold py-2 bg-blue-700 rounded-lg text-white cursor-pointer "
-        >
-          Verify
-        </button>
-      </div>
-    </>
+    <Suspense fallback="loading...">
+      <VerifyEmailClient />
+    </Suspense>
   );
 };
 
-export default VerifyEmail;
+export default page;
